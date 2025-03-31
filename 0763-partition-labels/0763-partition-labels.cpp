c932e549b81b010147d7198248ea104f90e77052
Diksha Mehta <33371089+dikshamehta@users.noreply.github.com>
2025-03-31 10:33:52 -0400

    Time: 3 ms (30.87%), Space: 9.1 MB (15.58%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,49 +1,25 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        // Stores the last index of each character in 's'
-        vector<int> lastOccurrence(26, 0);
-        for (int i = 0; i < s.size(); ++i) {
-            lastOccurrence[s[i] - 'a'] = i;
+        unordered_map<char, int> lastIndex;
+        vector<int> res;
+        
+        // Store the last occurrence of each character
+        for (int i = 0; i < s.size(); i++) {
+            lastIndex[s[i]] = i;
         }
-
-        int partitionEnd = 0, partitionStart = 0;
-        vector<int> partitionSizes;
-        for (int i = 0; i < s.size(); ++i) {
-            partitionEnd = max(partitionEnd, lastOccurrence[s[i] - 'a']);
-            // End of the current partition
-            if (i == partitionEnd) {
-                partitionSizes.push_back(i - partitionStart + 1);
-                partitionStart = i + 1;
+        
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            end = max(end, lastIndex[s[i]]);
+            
+            if (i == end) {  // When we reach the end of a partition
+                res.push_back(end - start + 1);
+                start = i + 1;
             }
         }
-        return partitionSizes;
+        
+        return res;
     }
 };
-
-// class Solution {
-// public:
-//     vector<int> partitionLabels(string s) {
-//         unordered_map<char, int> freq;
-//         vector<int> res;
-//         int p=0;
-
-//         for(int i=0; i<s.size(); i++){
-            
-//             if(freq.find(s[i])!=freq.end()){
-//                 //find the key
-//                 if(freq[s[i]]<res.back()){
-//                     res.back() = i;
-//                 }
-                
-//             }
-//             else{
-//                 p = i;
-//                 res.push_back(p);
-//             }
-
-//             freq[s[i]] = i;
-//         }
-//         return res;
-//     }
-// };
