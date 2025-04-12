0cee59cf14dd011e708d3a63c74052846c1355ff
Diksha Mehta <33371089+dikshamehta@users.noreply.github.com>
2025-04-12 13:19:23 -0400

    Time: 35 ms (6.81%), Space: 10.7 MB (93.14%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,39 +1,63 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int max_len = 0;
-        int n = s.size();
-
-        for(int c='A'; c<='Z'; c++){
-            int i=0; 
-            int j=0;
-            int remaining_k = k;
-
-            while(j<n){
-                if(s[j] == c){
+        int ans = 0, n = s.size();
+        for(char c = 'A'; c <= 'Z'; c++)
+        {
+            int i = 0, j = 0, replaced = 0;
+            while(j < n)
+            {
+                if(s[j] == c)
                     j++;
-                }
-                else if(remaining_k>0){
-                    j++; remaining_k--;
-                }
-                // Now in our above condition we know k is finished, we can not move further
-                // replacing elements because, k<0 and s[j]!=c
-                // So, now we have to move to left side of window and shrink it
-                // so now checking if, we did use a k for ith position
-                else if(s[i] == c){
-                    // if ith element is already c then we did not use k
-                    // then just move forward
+                else if(replaced < k)
+                    j++, replaced++;
+                else if(s[i] == c)
                     i++;
-                }
-                else{
-                    //but if it was not c then we used k for it
-                    //now we are leaving that position so restore k
-                    i++; remaining_k++;
-                }
-
-                max_len = max(max_len, j-i);
+                else
+                    i++, replaced--;
+                ans = max(ans, j - i);
             }
         }
-        return max_len;  
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+//         int max_len = 0;
+//         int n = s.size();
+
+//         for(int c='A'; c<='Z'; c++){
+//             int i=0; 
+//             int j=0;
+//             int remaining_k = k;
+
+//             while(j<n){
+//                 if(s[j] == c){
+//                     j++;
+//                 }
+//                 else if(remaining_k>0){
+//                     j++; remaining_k--;
+//                 }
+//                 // Now in our above condition we know k is finished, we can not move further
+//                 // replacing elements because, k<0 and s[j]!=c
+//                 // So, now we have to move to left side of window and shrink it
+//                 // so now checking if, we did use a k for ith position
+//                 else if(s[i] == c){
+//                     // if ith element is already c then we did not use k
+//                     // then just move forward
+//                     i++;
+//                 }
+//                 else{
+//                     //but if it was not c then we used k for it
+//                     //now we are leaving that position so restore k
+//                     i++; remaining_k++;
+//                 }
+
+//                 max_len = max(max_len, j-i);
+//             }
+//         }
+//         return max_len;  
+//     }
+// };
