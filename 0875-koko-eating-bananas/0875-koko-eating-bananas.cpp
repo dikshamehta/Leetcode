2201fb85c5bbdb5c7e924917c59c1d0409fa0f9d
Diksha Mehta <33371089+dikshamehta@users.noreply.github.com>
2025-05-27 09:31:22 -0400

    Time: 22 ms (8.51%), Space: 23.2 MB (7.61%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+
+        int res = r; 
+
+        while(l<=r){
+            int m = (l+r)/2;
+
+            double totaltime = 0;
+
+            for(int p : piles){
+                totaltime += ceil(p / (double) m); //converting one of them to double to have floating division
+            }
+
+            cout<<totaltime<<" ";
+            if(totaltime <= h){
+                res = m;
+                r = m-1;
+            }
+            else{
+                l = m+1;
+            }
+        }
+        return res;
+    }
+};
