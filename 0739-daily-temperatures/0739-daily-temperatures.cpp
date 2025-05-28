aea8858ad367ddfa7bf59c0a500d8114e137b8b7
Diksha Mehta <33371089+dikshamehta@users.noreply.github.com>
2025-05-28 10:06:04 -0400

    Time: 26 ms (45.02%), Space: 107.3 MB (42.1%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // to track the index of elements till next warmer weather is reached
         stack<int> temp;
         vector<int> res(temperatures.size(), 0);
         for(int i=0; i<temperatures.size(); i++){
+
+            // this loop is to calculate after how many days we got warmer weather for current index
             while(!temp.empty() && temperatures[temp.top()] < temperatures[i]){
                 res[temp.top()] = i - temp.top();
                 temp.pop();
             }
+
+            // push all the index in the stack
             temp.push(i);
         }
 
