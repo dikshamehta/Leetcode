76c5abb204c5743985065f39de86c4b81ff1e739
Diksha Mehta <33371089+dikshamehta@users.noreply.github.com>
2025-05-31 20:09:29 -0400

    Time: 35 ms (91.68%), Space: 137.1 MB (40.32%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,18 +1,55 @@
 class TimeMap {
 public:
-    unordered_map<string, map<int, string>> m;
+    unordered_map<string, vector<pair<int, string>>> mp;
 
     TimeMap() {}
 
     void set(string key, string value, int timestamp) {
-        m[key].insert({timestamp, value});
+        mp[key].emplace_back(timestamp, value); //mp[key].push_back(make_pair(timestamp, value));
     }
 
     string get(string key, int timestamp) {
-        auto it = m[key].upper_bound(timestamp);
-        return it == m[key].begin() ? "" : prev(it)->second;
+        // values is the vector with pairs of (timestamp, value)
+        auto& values = mp[key];
+
+        int l = 0;
+        int h = values.size() - 1;
+
+        string result = "";
+
+        while(l<=h){
+            int m = (l + h)/2;
+
+            if(values[m].first <= timestamp){
+                result = values[m].second;
+                l = m+1;
+            }
+            else{
+                h = m-1;
+            }
+        }
+
+        return result;
+
     }
 };
+
+
+// class TimeMap {
+// public:
+//     unordered_map<string, map<int, string>> m;
+
+//     TimeMap() {}
+
+//     void set(string key, string value, int timestamp) {
+//         m[key].insert({timestamp, value});
+//     }
+
+//     string get(string key, int timestamp) {
+//         auto it = m[key].upper_bound(timestamp);
+//         return it == m[key].begin() ? "" : prev(it)->second;
+//     }
+// };
 
 /**
  * Your TimeMap object will be instantiated and called as such:
