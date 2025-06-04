420e4aac46af4cb4fe7918843c30d86abe5b2cbf
Diksha Mehta <33371089+dikshamehta@users.noreply.github.com>
2025-06-04 18:41:30 -0400

    Time: 0 ms (100%), Space: 17.6 MB (52.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,87 @@
 class Solution {
 public:
-    int findFirst(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1, index = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+    int lowerBound(vector<int>& nums, int target, int n){
+        int low = 0;
+        int high = n-1;
+        int res = n;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(nums[mid]>=target){
+                res = mid;
+                high = mid-1;
+                
             }
-            if (nums[mid] == target) index = mid;
+            else{
+                low = mid + 1;
+            }
         }
-        return index;
+        return res;
     }
 
-    int findLast(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1, index = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+    int upperBound(vector<int>& nums, int target, int n){
+        int low = 0;
+        int high = n-1;
+        int res = n;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(nums[mid]>target){
+                res = mid;
+                high = mid-1;
+                
             }
-            if (nums[mid] == target) index = mid;
+            else{
+                low = mid + 1;
+            }
         }
-        return index;
+        return res;
     }
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
-        return {first, last};
+        int n = nums.size();
+        int lb = lowerBound(nums, target, n);
+        
+        if(lb == n || nums[lb]!= target) return {-1,-1};
+        return {lb, upperBound(nums, target, n)-1};
     }
 };
+
+// class Solution {
+// public:
+//     int findFirst(vector<int>& nums, int target) {
+//         int left = 0, right = nums.size() - 1, index = -1;
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] >= target) {
+//                 right = mid - 1;
+//             } else {
+//                 left = mid + 1;
+//             }
+//             if (nums[mid] == target) index = mid;
+//         }
+//         return index;
+//     }
+
+//     int findLast(vector<int>& nums, int target) {
+//         int left = 0, right = nums.size() - 1, index = -1;
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] <= target) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid - 1;
+//             }
+//             if (nums[mid] == target) index = mid;
+//         }
+//         return index;
+//     }
+
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int first = findFirst(nums, target);
+//         int last = findLast(nums, target);
+//         return {first, last};
+//     }
+// };
