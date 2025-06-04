e66e6ae694c5bbaf5bfffc73ae830634b7715db6
Diksha Mehta <33371089+dikshamehta@users.noreply.github.com>
2025-06-04 19:02:59 -0400

    Time: 0 ms (100%), Space: 17.7 MB (17.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,121 @@
+// if not using lower and upper bound concept and use simple binary search
 class Solution {
 public:
-    int lowerBound(vector<int>& nums, int target, int n){
+    int findFirst(vector<int>& nums, int target, int n){
         int low = 0;
         int high = n-1;
-        int res = n;
+        int first = -1;
+
+        // with this ran into issue of not handling -1, -1 correctly
+        // because first is getting update even if we didnt find the number
+        // therefore adding condition to check nums[mid] == target
+        // while(low<=high){
+        //     int mid = (low+high)/2;
+        //     if(nums[mid] >= target){
+        //         high = mid-1;
+        //         first = mid;
+        //     }
+        //     else{
+        //         low = mid+1;
+        //     }
+        // }
 
         while(low<=high){
             int mid = (low+high)/2;
-
-            if(nums[mid]>=target){
-                res = mid;
+            if(nums[mid] == target){
+                first = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] > target){
                 high = mid-1;
-                
             }
             else{
-                low = mid + 1;
+                low = mid+1;
             }
         }
-        return res;
+
+        return first;
     }
 
-    int upperBound(vector<int>& nums, int target, int n){
+    int findLast(vector<int>& nums, int target, int n){
         int low = 0;
         int high = n-1;
-        int res = n;
+        int last = -1;
 
         while(low<=high){
             int mid = (low+high)/2;
 
-            if(nums[mid]>target){
-                res = mid;
-                high = mid-1;
-                
-            }
-            else{
+            if(nums[mid] == target){
+                last = mid;
                 low = mid + 1;
             }
+            else if(nums[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
         }
-        return res;
+        return last;
     }
 
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int lb = lowerBound(nums, target, n);
-        
-        if(lb == n || nums[lb]!= target) return {-1,-1};
-        return {lb, upperBound(nums, target, n)-1};
+        return {findFirst(nums, target, n), findLast(nums, target, n)};
     }
 };
+
+// Solved using Lower and Upper Bound Concept
+// class Solution {
+// public:
+//     int lowerBound(vector<int>& nums, int target, int n){
+//         int low = 0;
+//         int high = n-1;
+//         int res = n;
+
+//         while(low<=high){
+//             int mid = (low+high)/2;
+
+//             if(nums[mid]>=target){
+//                 res = mid;
+//                 high = mid-1;
+                
+//             }
+//             else{
+//                 low = mid + 1;
+//             }
+//         }
+//         return res;
+//     }
+
+//     int upperBound(vector<int>& nums, int target, int n){
+//         int low = 0;
+//         int high = n-1;
+//         int res = n;
+
+//         while(low<=high){
+//             int mid = (low+high)/2;
+
+//             if(nums[mid]>target){
+//                 res = mid;
+//                 high = mid-1;
+                
+//             }
+//             else{
+//                 low = mid + 1;
+//             }
+//         }
+//         return res;
+//     }
+
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int lb = lowerBound(nums, target, n);
+        
+//         if(lb == n || nums[lb]!= target) return {-1,-1};
+//         return {lb, upperBound(nums, target, n)-1};
+//     }
+// };
 
 // class Solution {
 // public:
